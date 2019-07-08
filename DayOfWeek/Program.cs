f884bfbd28169997c33747316b5f1be0017b84cb
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-08 21:54:02 +0200

    Add project files.

---
+++ DayOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program gets the day of the week for a specified date.");
+            System.DateTime dayOfBirth = new DateTime(2019, 5, 17);
+            Console.WriteLine("The day of the week {0} is: {1}", dayOfBirth, dayOfBirth.DayOfWeek);
+
+            Console.ReadKey();
+        }
+    }
+}
